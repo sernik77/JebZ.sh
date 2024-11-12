613403eee1c04c105281f75ab4f9fc918678d958
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2024-11-12 21:24:39 +0100

    Add files via upload

---
+++ Extras/Scripts/dependencies.js
@@ -0,0 +1,12 @@
+fetch("https://jbzd.com.pl/js/0.e2457a0cd1432ea83dbc.js", {
+  "headers": {
+    "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "referer": "https://jbzd.com.pl/oczekujace",
+    "referrerPolicy": "strict-origin-when-cross-origin"
+  },
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+});
